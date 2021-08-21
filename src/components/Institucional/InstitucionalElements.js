b8711542fae72cc1ef307d9f1cf90b2830f1c92b
Ivo Netto <78481345+ivonetto@users.noreply.github.com>
2021-08-21 15:00:27 -0300

    Adição Institucional e ButtonElement

--- src/components/Institucional/InstitucionalElements.js
+++ src/components/Institucional/InstitucionalElements.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { MdKeyboardArrowRight, MdArrowForward } from 'react-icons/md';
 
 export const InstContainer = styled.div`
     background: #e4e5e6;
@@ -9,9 +10,19 @@ export const InstContainer = styled.div`
     height: 800px;
     position: relative;
     z-index: 1;
-`;
 
-/* Add :before styles */
+    :before{
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background: linear-gradient(180deg, rgba(0,0,0,0.2) 0%, rgba(0,0,0,0.6) 100%),
+        linear-gradient(180deg, rgba(0,0,0,0.2) 0%, transparent 100%);
+        z-index: 2;
+    }
+`;
 
 export const InstBg = styled.div`
     position: absolute;
@@ -31,3 +42,60 @@ export const VideoBg = styled.video`
     object-fit: cover;
     background: #232a34;
 `;
+
+export const InstContent = styled.div`
+    z-index: 3;
+    max-width: 1200px;
+    position: absolute;
+    padding: 8px 24px;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const InstH1 = styled.h1`
+    color: #e4e5e6;
+    font-size: 48px;
+    text-align: center;
+
+    @media screen and (max-width: 768px){
+        font-size: 40px;
+    }
+
+    @media screen and (max-width: 480px){
+        font-size: 32px;
+    }
+`;
+
+export const InstP = styled.p`
+    margin-top: 24px;
+    color: #e4e5e6;
+    font-size: 24px;
+    text-align: center;
+    max-width: 600px;
+
+    @media screen and (max-width: 768px){
+        font-size: 24px;
+    }
+
+    @media screen and (max-width: 768px){
+        font-size: 18px;
+    }
+`;
+
+export const InstBtnWrapper = styled.div`
+    margin-top: 32px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const ArrowForward = styled(MdArrowForward)`
+    margin-left: 8px;
+    font-size: 20px;
+`;
+
+export const ArrowRight = styled(MdKeyboardArrowRight)`
+    margin-left: 8px;
+    font-size: 20px;
+`;
+
--- src/components/Institucional/index.js
+++ src/components/Institucional/index.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Video from '../../videos/video.mp4';
-import { InstContainer, InstBg, VideoBg } from './InstitucionalElements';
+import { Button } from '../ButtonElement';
+import {
+    InstContainer,
+    InstBg,
+    VideoBg,
+    InstContent,
+    InstH1,
+    InstP,
+    InstBtnWrapper,
+    ArrowForward,
+    ArrowRight
+} from './InstitucionalElements';
 
 const Institucional = () => {
+
+    const [hover, setHover] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
     return (
         <InstContainer>
             <InstBg>
                 <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
             </InstBg>
+            <InstContent>
+                <InstH1>Institucional</InstH1>
+                <InstP>
+                    Somos uma editora criada por professores,
+                    constituída com profissionais experientes e atuantes no desenvolvimento
+                    e produção de conteúdos didáticos. Temos como princípio contribuir
+                    com as escolas em seus processos de preparacão e capacitação dos estudantes,
+                    objetivando o aprendizado e a conquista de índices de aprovação expressivos.
+                </InstP>
+                <InstBtnWrapper>
+                    <Button to='produtos' onMouseEnter={onHover} onMouseLeave={onHover}
+                        primary="true"
+                        gray="true"
+                    >
+                        Nossos materiais {hover ? <ArrowForward /> : <ArrowRight/>}
+                    </Button>
+                </InstBtnWrapper>
+            </InstContent>
         </InstContainer>
     )
 }
